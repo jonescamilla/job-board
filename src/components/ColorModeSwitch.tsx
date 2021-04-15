bb360162e3e4b5bd2f0a55bab664f0f87c38dafc
jon.escamilla <jonathanescamilla1@gmail.com>
2021-04-15 11:19:44 -0700

    component that toggles the color mode used througout the app; light and dark theme

---
+++ src/components/ColorModeSwitch.tsx
@@ -0,0 +1,29 @@
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+import {
+  Button,
+  Tooltip,
+  useColorMode,
+  VisuallyHidden,
+} from '@chakra-ui/react';
+
+/**
+ * toggle component that handles logic for toggling colorMode used throughout application
+ */
+export const ColorModeSwitch = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const isDark = colorMode === 'dark';
+  return (
+    <Tooltip hasArrow label="toggle color mode">
+      <Button
+        bg=""
+        position="fixed"
+        top="1rem"
+        right="1rem"
+        onClick={toggleColorMode}
+      >
+        <VisuallyHidden>Color Mode Toggle</VisuallyHidden>
+        {isDark ? <SunIcon /> : <MoonIcon />}
+      </Button>
+    </Tooltip>
+  );
+};
