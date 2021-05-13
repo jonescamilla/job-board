039a2360065afce3fb751e7b8f89f0f8c233dc44
jon.escamilla <jonathanescamilla1@gmail.com>
2021-05-13 15:25:41 -0700

    adjusted formatting of code and improved typing/pseudo code/comments

--- src/api.ts
+++ src/api.ts
@@ -1,25 +1,49 @@
 import axios from 'axios';
-
-type header = Partial<Record<'description' | 'location' | 'full_time', string>>;
+import { paramsFunc } from './types';
 
 /**
  * `class` containing all mentions of the `GitHub Jobs API`
  */
-
 class GHJobsApi {
   private static API_URL = 'https://jobs.github.com';
 
   /**
    * returns axios `get` promise to `/positions.json` from GitHub Jobs Api
-   *
-   * @param description A search term, such as 'ruby' or 'java'.
-   * @param location A city name, zip code, or other location search term.
-   * @param full_time If you want to limit results to full time positions set this parameter to 'true'.
    */
-  public static async positions({ description, location, full_time }: header) {
+  public static positions: paramsFunc<Promise<any>> = async (
+    description,
+    location,
+    full_time
+  ) => {
+    const formattedParams = GHJobsApi.filterParams(
+      description,
+      location,
+      full_time
+    );
+
     return axios({
       method: 'get',
-      url: `${this.API_URL}/positions.json`,
+      url: `${GHJobsApi.API_URL}/positions.json${formattedParams}`,
+    })
+      .then((result) => {
+        console.log(result.status, result.statusText);
+        return result.data;
+      })
+      .catch((error) => {
+        console.log(error.status, error.message);
+        return null;
+      });
+  };
+
+  /**
+   * returns axios `get` promise to `/positions/ID.json` from GitHub Jobs Api
+   *
+   * @param markdown Set to 'true' to get the description and `how_to_apply` fields as Markdown.
+   */
+  public static async positionById(id: string, markdown?: boolean) {
+    return axios({
+      method: 'get',
+      url: `${this.API_URL}/positions/${id}.json`,
     })
       .then((result) => {
         console.log(result.status, result.statusText);
@@ -32,21 +56,32 @@ class GHJobsApi {
   }
 
   /**
-   * returns axios `get` promise to `/positions/ID.json` from GitHub Jobs Api
-   * @param markdown Set to 'true' to get the description and `how_to_apply` fields as Markdown.
+   * formats params entered by user to proper api formatting
+   *
+   * @todo there are better ways to implement this, needs improvements
    */
-  public static async positionById({
-    id,
-    markdown,
-  }: {
-    id: string;
-    markdown?: boolean;
-  }) {
-    return axios({
-      method: 'get',
-      url: `${this.API_URL}/positions${id}.json`,
-    });
-  }
+  private static filterParams: paramsFunc<string> = (
+    description,
+    location,
+    full_time
+  ) => {
+    // array to hold strings of params
+    let args = [];
+    if (description != '' && description != undefined) {
+      args.push('description=' + description);
+    }
+    if (location !== undefined && location != '') {
+      args.push('location=' + location);
+    }
+    if (full_time != undefined && full_time != false) {
+      args.push('full_time=' + full_time);
+    }
+    return args.reduce((str, key, index) => {
+      if (index === 0) str += '?';
+      str != '?' ? (str += `&${key}`) : (str += key);
+      return str;
+    }, '');
+  };
 }
 
 export default GHJobsApi;
