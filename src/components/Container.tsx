02020df2fdab61376a7a522250b3a71e3bcfe6ac
jon.escamilla <jonathanescamilla1@gmail.com>
2021-05-13 15:35:32 -0700

    improved comments

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -3,7 +3,10 @@ import { Box, useColorMode, useColorModeValue } from '@chakra-ui/react';
 /**
  * function that returns a `JSX.Element` with pre-configured `JSX Attributes`
  * @param {any} props any children you wish to render within Container or JSX attributes you wish to add
+ *
+ * used to handle theme toggling and generic application theming
  */
+
 export const Container = (props: any) => {
   const bg = useColorModeValue('bg.light', 'bg.dark');
   const color = useColorModeValue('black', 'white');
