9b7464901d54eb404e258432498772a240551bdb
jon.escamilla <jonathanescamilla1@gmail.com>
2021-05-13 15:26:19 -0700

    add new types used in front end and api with improved jsdoc

--- src/types.ts
+++ src/types.ts
@@ -11,3 +11,23 @@ export interface Position {
   how_to_apply: string;
   company_logo?: string | null;
 }
+
+export interface positionParameters {
+  /** A search term, such as "ruby" or "java". This parameter is aliased to search */
+  description: string;
+  /** A city name, zip code, or other location search term */
+  location: string;
+  /** If you want to limit results to full time positions set this parameter to 'true' */
+  full_time: boolean;
+}
+
+/**
+ * @param description A search term, such as 'ruby' or 'java'.
+ * @param location A city name, zip code, or other location search term.
+ * @param full_time If you want to limit results to full time positions set this parameter to 'true'.
+ */
+export type paramsFunc<R> = (
+  description?: string,
+  location?: string,
+  full_time?: boolean
+) => R;
